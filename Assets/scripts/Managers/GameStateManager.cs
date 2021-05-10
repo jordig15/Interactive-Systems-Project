8491b280f1a14ee3bd0defe7c2a262995d2df7a4
magmarc-9 <magmarc9@gmail.com>
2021-05-11 00:14:32 +0200

    1r commit 11/05 0:14

---
+++ Assets/scripts/Managers/GameStateManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance;
+    public GameObject redPlayer;
+    public GameObject bluePlayer;
+    public float playersDistanceThresh;
+
+    private bool playersInContact;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.playersInContact = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 redPlayerPos = this.redPlayer.transform.position;
+        Vector3 bluePlayerPos = this.bluePlayer.transform.position;
+        float dist = Vector3.Distance(redPlayerPos, bluePlayerPos);
+
+        playersInContact = false;
+        if (dist <= playersDistanceThresh)
+        {
+            playersInContact = true;
+        }
+
+        Debug.Log(playersInContact);
+    }
+}
