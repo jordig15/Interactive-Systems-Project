98bc89d1e430dee352f912c6b61e727dda44af99
magmarc-9 <magmarc9@gmail.com>
2021-06-02 22:24:56 +0200

    02/06 22:24

---
+++ Assets/scripts/CatchDropObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteractionç : MonoBehaviour
+{
+
+    public GameObject interactiveObject;
+    public float cooldown = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
