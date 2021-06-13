6cd634efb738e482fe01f2d0f9b035eac3c40ce7
jordig15 <jordi.guitart02@estudiant.upf.edu>
2021-06-13 15:31:59 +0200

    aaaa
    aaaaa

---
+++ Assets/scripts/jhonRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jhonRotate : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    public float angle = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0f, -1f, 0f) * rotationSpeed);
+    }
+}
